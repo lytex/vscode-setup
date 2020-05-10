0d0da00dcd0e8c0ab249c879a19a2070a45ac4bb
lytex <julianlpc15@gmail.com>
2020-05-10 19:10:33 +0200

    improve compare_and_merge and print stdout

--- compare_and_merge.sh
+++ compare_and_merge.sh
@@ -4,6 +4,9 @@ if [ -e $1 ]; then
     echo "file $1 exists. Copying to $1.old and merging"
     cp $1 $1.old
     git merge-file -L user -L '' -L vscode-setup $1 .empty $2
+    if (($? != 0)); then
+        echo "MERGE CONFLICT Check file $1 or restore from $1.old"
+    fi
 else
     cp $2 $1
 fi
