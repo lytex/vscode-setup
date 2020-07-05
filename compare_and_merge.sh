7c749076e556a88dc4dcb1c7e1b5f962ad86fab5
lytex <julianlpc15@gmail.com>
2020-07-05 18:56:52 +0200

    iterative add .old

--- compare_and_merge.sh
+++ compare_and_merge.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 echo '' > .empty # git merge-file needs a common file
+backup=$1
+
 if [ -e $1 ]; then
-    echo "file $1 exists. Copying to $1.old and merging"
-    cp $1 $1.old
-    git merge-file -L user -L '' -L vscode-setup $1 .empty $2
+    while [ -e $backup ]; do
+        backup="$backup.old" # Add .old until the file doesn't exist to avoid overwriting
+    done
+    echo "file $1 exists. Copying to $backup and merging"
+    cp $1 $backup
+    git merge-file -L user -L '' -L vscode-setup $1.empty $2
     if (($? != 0)); then
-        echo "MERGE CONFLICT Check file $1 or restore from $1.old"
+        echo "MERGE CONFLICT Check file $1 or restore from $backup"
     fi
 else
     cp $2 $1
