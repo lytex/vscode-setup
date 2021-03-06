2f60d8c554994f028f65fecb07bfc8e89db5263f
lytex <julianlpc15@gmail.com>
2021-03-06 15:19:33 +0100

    remove color profile

--- mkvscode_full-activate.sh
+++ mkvscode_full-activate.sh
@@ -6,14 +6,14 @@ for venv in $venvs; do
 	venv_script="$venv/bin/full-activate"
 	if [ ! -f "$venv_script" ]; then
 		echo "Creating full-activate script at $venv_script"
-		echo -e ". $venv/bin/activate\nipython3 --profile=color" > "$venv_script"
+		echo -e ". $venv/bin/activate\nipython3" > "$venv_script"
 	else
 		echo "File already exists at $venv/bin/full-activate"
                 echo "Do you whant to replace it? [y/N]"
                 read replace
                 if [ $replace == "y" -o $replace == "Y" ]; then
                     rm "$venv/bin/full-activate"
-		    echo -e ". $venv/bin/activate\nipython3 --profile=color" > "$venv_script"
+		    echo -e ". $venv/bin/activate\nipython3" > "$venv_script"
                 fi
 	fi
 
